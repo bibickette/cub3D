dd634a851100b5c833e3ff198b08ca5446964cfa
Fanny Salomon <fsalomon@paul-f5Br6s6.clusters.42paris.fr>
2025-01-21 14:24:43 +0100

    optimisation fonction

--- src/minimap/draw_circle_minimap.c
+++ src/minimap/draw_circle_minimap.c
@@ -6,13 +6,13 @@
 /*   By: fsalomon <fsalomon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 13:49:34 by fsalomon          #+#    #+#             */
-/*   Updated: 2025/01/21 14:21:03 by fsalomon         ###   ########.fr       */
+/*   Updated: 2025/01/21 14:23:42 by fsalomon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-static void	draw_circle_map_border(t_parsing *info, int xc, int yc, int radius)
+static void	draw_circle_map(t_parsing *info, int radius, int color)
 {
 	int	x;
 	int	y;
@@ -24,28 +24,8 @@ static void	draw_circle_map_border(t_parsing *info, int xc, int yc, int radius)
 		while (x < radius)
 		{
 			if (x * x + y * y < radius * radius)
-				my_mlx_pixel_put(*info->mlx.current_background, xc + x, yc + y,
-					OTHER_PINK);
-			x++;
-		}
-		y++;
-	}
-}
-
-static void	draw_circle_map(t_parsing *info, int xc, int yc, int radius)
-{
-	int	x;
-	int	y;
-
-	y = -radius;
-	while (y < radius)
-	{
-		x = -radius;
-		while (x < radius)
-		{
-			if (x * x + y * y < radius * radius)
-				my_mlx_pixel_put(*info->mlx.current_background, xc + x, yc + y,
-					info->textures.floor_color);
+				my_mlx_pixel_put(*info->mlx.current_background, MINI_MAP_CENTER
+					+ x, MINI_MAP_CENTER + y, color);
 			x++;
 		}
 		y++;
@@ -54,8 +34,7 @@ static void	draw_circle_map(t_parsing *info, int xc, int yc, int radius)
 
 void	draw_mini_map(t_parsing *info)
 {
-	draw_circle_map_border(info, MINI_MAP_CENTER, MINI_MAP_CENTER,
-		MINI_MAP_RAY_BORDER);
-	draw_circle_map(info, MINI_MAP_CENTER, MINI_MAP_CENTER, MINI_MAP_RAY);
+	draw_circle_map(info, MINI_MAP_RAY_BORDER, OTHER_PINK);
+	draw_circle_map(info, MINI_MAP_RAY, info->textures.floor_color);
 	draw_player_arrow(info);
 }
